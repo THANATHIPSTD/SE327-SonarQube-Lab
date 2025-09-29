5cae73d13c7726ebdebee0003b133b7fd029c4c0
THANATHIPSTD <thanathip_suj@cmu.ac.th>
2025-09-29 13:36:16 +0700

    Add test corverage

--- src/test/java/se331/CalculatorTest.java
+++ src/test/java/se331/CalculatorTest.java
@@ -1,33 +1,19 @@
 package se331;
 
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class CalculatorTest {
+class AdvancedCalculatorTest {
 
     @Test
-    public void testAdd() {
-        Calculator calculator = new Calculator();
-        assertEquals(5, calculator.add(2, 3));
+    void testPower() {
+        AdvancedCalculator calculator = new AdvancedCalculator();
+        assertEquals(8.0, calculator.power(2, 3), 0.01);
     }
 
     @Test
-    public void testSubtract() {
-        Calculator calculator = new Calculator();
-        assertEquals(1, calculator.subtract(5, 4));
-    }
-
-    @Test
-    public void testMultiply() {
-        Calculator calculator = new Calculator();
-        assertEquals(6, calculator.multiply(2, 3));
-    }
-
-    @Test
-    public void testDivide() {
-        Calculator calculator = new Calculator();
-        assertEquals(2.0, calculator.divide(4, 2), 0.01);
+    void testSqrt() {
+        AdvancedCalculator calculator = new AdvancedCalculator();
+        assertEquals(2.0, calculator.sqrt(4), 0.01);
     }
 }
